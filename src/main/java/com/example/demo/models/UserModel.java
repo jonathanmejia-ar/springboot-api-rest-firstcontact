c6c86a577f8b1f59c5481dd70b3f867d05f23d07
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-05-02 13:02:32 -0300

    feat: User model

---
+++ src/main/java/com/example/demo/models/UserModel.java
@@ -0,0 +1,41 @@
+package com.example.demo.models;
+
+public class UserModel {
+    private Long id;
+    private String name;
+    private String email;
+    private Integer priority;
+
+    public void setPriority(Integer priority){
+        this.priority = priority;
+    }
+
+    public Integer getPriority(){
+        return this.priority;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+}
